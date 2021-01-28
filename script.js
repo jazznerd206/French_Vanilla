8c2d57c119d0b2b08385c5b71f568a0a38d98b5d
Andrew Miller <jazznerd206@icloud.com>
2021-01-28 14:51:37 -0800

    add hover button reveal box

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('hello world');
+
+// BUTTON REVEAL MESSAGE SCRIPT
+document.addEventListener('DOMContentLoaded', function(){ 
+    var tail = document.querySelector('.tail');
+    var height = tail.scrollHeight();
+    tail.style.setProperty('--max-height', height + 'px');
+}, false);
+// ============================
+
