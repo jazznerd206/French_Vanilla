7764d41932f9af4e2f537566a164d3cf9eabd644
Andrew Miller <jazznerd206@icloud.com>
2021-02-06 18:04:29 -0800

    add css

--- Image_Panels/script.js
+++ Image_Panels/script.js
@@ -5,24 +5,32 @@ const panels = document.getElementsByClassName('panels');
 console.log(img_panels, panels)
 
 const pxl8 = () => {
-    for (let i = 1; i < 900; i++) {
+    for (let i = 1; i < 625; i++) {
         img_panels.innerHTML += "<div class='panels'></div>";
-        const duration = Math.random() * 5;
+        const duration = Math.random() * 1.1;
         panels[i].style.animationDuration = 2 + duration +'s';
         panels[i].style.animationDelay = duration +'s';
     };
 }
+
+
 pxl8();
 
 let images = [
-    './assets/IMG_0603.png', './assets/IMG_0805.png', './assets/IMG_0101.png'
+    './assets/IMG_0101.png', './assets/IMG_0101.png', './assets/IMG_0805.png'
 ]
+
+
 const section = document.querySelector('section');
 const img = document.getElementById('chief')
+
 let count = 0;
 setTimeout(() => {
     if (count > images.length) count = 0;
+    console.log(img)
+    console.log(images[count])
     img.setAttribute('src', images[count])
-    pxl8()
+    console.log(img)
+    // pxl8();
     count++;
-}, 14000); // add class active after 14s
+}, 6000); // add class active after 14s
