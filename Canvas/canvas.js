21a9c7f66dc522601ba49d99042986539fd5d6c4
Andrew Miller <jazznerd206@icloud.com>
2021-02-14 14:57:31 -0800

    add vine animation

--- Canvas/canvas.js
+++ Canvas/canvas.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
+const PI = Math.PI
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -14,107 +15,95 @@ window.addEventListener(
   false
 );
 
-class Trunk {
-    constructor(start, height, width, angle) {
-        this.start = start;
-        this.height = height;
-        this.width = width;
-        this.angle = angle
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+const colors = [
+    "#4cff4c",
+    "#65ff65",
+    "#7fff7f",
+    "#98ff98",
+    "#b2ffb2",
+    "#22ff22",
+    "#003600",
+    "#009800",
+    "#00d300",
+    "#4cff4c",
+    "#65ff65",
+    "#7fff7f",
+    "#98ff98",
+    "#b2ffb2",
+    "#22ff22",
+    "#003600",
+    "#009800",
+    "#00d300"
+]
+
+function Trunk(props) {
+    let { x, y, thickness, width, loss, direction, angle, color } = props;
+    
+    const probability = .9;
+    const amplitude = .025;
+
+    // draw
+    context.beginPath();
+    context.moveTo(x, y);
+    
+    x += Math.cos(angle) * width;
+    y += -Math.sin(angle) * width;
+    
+    context.strokeStyle = color;
+    context.lineWidth = thickness;
+    context.lineDashOffset = [50,5];
+    context.lineTo(x, y);
+    context.stroke();
+    // context.closePath()
+    if (direction) {
+        angle -= amplitude
+    } else {
+        angle += amplitude
+    }
+    
+    if (Math.random() >= probability) {
+        Trunk({
+            x,
+            y,
+            thickness: thickness * .9,
+            width: 1.1 * width,
+            loss,
+            direction: !direction,
+            angle,
+            color
+        })
+    }
+    if (width > 1) {
+        setTimeout(() => {
+            Trunk({
+                x,
+                y,
+                thickness: thickness * .9,
+                width,
+                loss,
+                direction,
+                angle,
+                color
+            })
+        }, 50)
     }
 }
 
-class Stem {
-    constructor(x, y, length, width, angle) {
-        this.x = x;
-        this.y = y;
-        this.length = length;
-        this.width = width;
-        this.angle = angle;
-    }
-}
-
-
-const numTrunks = 1;
-let trunkArray = [];
-let randNum = Math.round(Math.random() * 10);
-
-
-const createTrunks = () => {
-    trunkArray = [];
-    for (let i = 1; i < numTrunks + 1; i++) {
-        let start = (canvas.width / (numTrunks + 1)) * i;
-        let h = 150 + Math.round(Math.random() * 50);
-        let w = 50;
-        w = randNum < 5 ? w + randNum : w - randNum;
-        let a = 10 + Math.round(Math.random()*50);
-        const addTrunk = new Trunk(start, h, w, a);
-        // console.log(addTrunk)
-        trunkArray.push(addTrunk);
-    }
-}
-createTrunks();
-
-let stemArray = [];
-const createStems = arr => {
-    stemArray = [];
-    let int = randNum
-    arr.forEach(trunk => {
-        for (let i = 0; i < 100; ++i) {
-            let x, y, length, width, angle;
-            x = trunk.start;
-            y = (canvas.height / 2 - trunk.height);
-            length = trunk.height * .5;
-            width = trunk.width / 2;
-            angle = -60;
-            let newStem = new Stem(x, y, length, width, angle * 1280)
-            stemArray.push(newStem)
-        }
+let numTrunks = 5;
+for (let i = 0; i < numTrunks; i++) {
+    let start = (canvas.width / (numTrunks + 1) * i);
+    Trunk({
+        x: start,
+        y: canvas.height + 10,
+        thickness: 20,
+        width: 35,
+        loss: .9,
+        direction: getRandom(0, 1),
+        angle: getRandom(0, 16) * PI / 16,
+        color: colors[i]
     })
 }
-createStems(trunkArray);
-
-function drawTrunks() {
-    for (let i = 0; i < trunkArray.length; ++i) {
-        console.log(trunkArray[i]);
-        let trunk = trunkArray[i];
-        context.beginPath();
-        context.moveTo(trunk.start, 0 - canvas.height / 2);
-        context.lineTo(trunk.start, canvas.height / 2 - trunk.height);
-        context.strokeStyle = 'red';
-        context.lineWidth = '10';
-        context.stroke();
-    }
-}
-
-function drawStems(arr) {
-    for (let i = 0; i < 100; ++i) {
-        let int = randNum
-        let stem = arr[i];
-        context.beginPath();
-        context.moveTo(stem.x, stem.y);
-        context.lineTo(stem.x + stem.length, stem.y + stem.length / i / 10);
-        context.strokeStyle = 'green';
-        context.lineWidth = '3';
-        context.stroke();
-    }
-}
-
-
-
-const draw = () => {
-    context.globalCompositeOperation = 'source-over';
-    context.fillStyle = 'rgb(249,215,28)';
-    context.fillRect(0, 0, canvas.width, canvas.height / 2);
-    drawTrunks();
-    // createStems(trunkArray)
-    console.log(stemArray)
-    drawStems(stemArray)
-}
-
-const init = () => {
-    // window.requestAnimationFrame(init);
-    draw();
-  }
-  
-init();
-
