ba1aeedcbee785c9160c899884bcfa311ec95449
Andrew Miller <jazznerd206@icloud.com>
2021-02-18 06:12:24 -0800

    add redirect

---
+++ JS/redirect.js
@@ -0,0 +1,4 @@
+// redirect
+// Redirects to a specified URL.
+
+// Use window.location.href or window.location.replace() to redirect to url. Pass a second argument to simulate a link click (true - default) or an HTTP redirect (false).
